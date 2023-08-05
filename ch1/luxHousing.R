d101ec50ecdf24f07ba6fd2f862c2f15e0dcf515
direnz <charlesdirenzo@gmail.com>
2023-08-05 14:18:39 -0500

    Update luxHousing.R
    added read_clean function

--- ch1/luxHousing.R
+++ ch1/luxHousing.R
@@ -8,4 +8,17 @@ library(janitor)
 
 #the url below oints to an excel file 
 #hosted on the book's github repository
-url <- 
+url <- "https://is.gd/1vvBAc"
+
+raw_data <- tempfile(fileext = ".xlsx")
+
+download.file(url, raw_data, method = "auto", mode = "wb")
+
+sheets <- excel_sheets(raw_data)
+
+#reads a specified sheet from an Excel file and then adds a new column to the data that contains the name or index of that sheet.
+read_clean <- function(..., sheet){
+  read_excel(..., sheet = sheet) |> 
+    mutate(year = sheet)
+}
+
