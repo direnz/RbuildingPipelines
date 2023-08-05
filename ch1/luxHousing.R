475b4fe412481000b49cbb26663b6e394aff0bfc
direnz <charlesdirenzo@gmail.com>
2023-08-05 14:48:40 -0500

    Update luxHousing.R

--- ch1/luxHousing.R
+++ ch1/luxHousing.R
@@ -22,3 +22,23 @@ read_clean <- function(..., sheet){
     mutate(year = sheet)
 }
 
+raw_data <- map(
+  sheets, 
+  ~read_clean(raw_data, 
+              skip = 10, 
+              sheet = .)
+                   ) |> 
+  bind_rows() |> 
+  clean_names()
+
+raw_data <- raw_data |>
+  rename(
+    locality = commune, 
+    n_offers = nombre_doffres, 
+    average_price_nominal_euros = prix_moyen_annonce_en_courant,
+    average_price_m2_nominal_euros = prix_moyen_annonce_au_m2_en_courant, 
+    average_price_m2_nominal_euros = prix_moyen_annonce_au_m2_en_courant
+  ) |> 
+  mutate(locality = str_trim(locality)) |>
+  select(year, locality, n_offers, starts_with("average"))
+
