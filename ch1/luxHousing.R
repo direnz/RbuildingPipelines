97f1e8194dda3dfcad32c655f1d029f11e783f71
direnz <charlesdirenzo@gmail.com>
2023-08-05 13:47:41 -0500

    Initial commit

---
+++ ch1/luxHousing.R
@@ -0,0 +1,10 @@
+#writing my own script from the Bruno Rodrigues book "building reproducible analyticl pipelines in R"
+
+library(dplyr)
+library(purrr)
+library(readxl)
+library(stringr)
+library(janitor)
+
+#the url below oints to an excel file 
+#hosted on the book's github repository
